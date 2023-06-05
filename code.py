71a9fefd57f7c49dd5cbf72be0f5b6fa6c8f96be
ElshanKU <116189498+ElshanKU@users.noreply.github.com>
2023-06-05 21:18:58 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import gzip
 
 # Load the compressed CSV file
@@ -12,21 +13,18 @@ df['date'] = pd.to_datetime(df['date'])
 # Filter data for January and February of 2024
 filtered_df = df[(df['date'].dt.month.isin([1, 2])) & (df['date'].dt.year == 2024)]
 
-# Extract month and year from 'date' column
+# Extract month from 'date' column
 filtered_df['month'] = filtered_df['date'].dt.strftime('%B')
 
 # Convert 'price' column to numeric values, ignore any non-numeric values
 filtered_df['price'] = pd.to_numeric(filtered_df['price'], errors='coerce')
 
-# Group data by month and calculate average price
-monthly_avg_prices = filtered_df.groupby('month')['price'].mean()
-
 # Define the custom order for months
 month_order = ['January', 'February']
 
-# Create a column chart using the average prices
+# Create a bar plot using seaborn
 plt.figure(figsize=(10, 6))
-plt.bar(monthly_avg_prices.index, monthly_avg_prices.values, color='skyblue')
+sns.barplot(data=filtered_df, x='month', y='price', order=month_order, color='skyblue')
 plt.xlabel('Months')
 plt.ylabel('Price ($)')
 plt.title('Average Prices of Listings in January and February 2024')
